6bb030d88f963ed885c87bb37867ab5ab06fc636
Jose Bello <bellojosema11@gmail.com>
2023-03-15 16:31:50 +0000

    Main Menu Print

---
+++ phase-1/phase_1.py
@@ -0,0 +1,7 @@
+# Print Main Menu
+print("\n\n\n")
+print("******** MAIN MENU ********")
+print("1. New Lifting Equipment")
+print("2. New Customer")
+print("3. New Lifting Equipment Rental")
+print("4. Exit")
