306435805e38c5cd033aae0854c7602c311d8ec3
Jose Bello <bellojosema11@gmail.com>
2023-03-15 16:58:20 +0000

    operational paths

--- phase-1/phase_1.py
+++ phase-1/phase_1.py
@@ -1,10 +1,13 @@
-# Print Main Menu
+# Prints Main Menu
 print("\n\n\n")
 print("******** MAIN MENU ********")
 print("1. New Lifting Equipment")
 print("2. New Customer")
 print("3. New Lifting Equipment Rental")
 print("4. Exit")
+
+# It prompts the user to choose one of the Main Menu choices between one and four to be executed.
+option = int(input("Please select an option (1-4): "))
 
 # option
 if option<1 or option>4:
