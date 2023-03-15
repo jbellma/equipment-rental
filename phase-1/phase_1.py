41de90476eedc51792cc5792c919ddc97a1e263a
Jose Bello <bellojosema11@gmail.com>
2023-03-15 16:52:33 +0000

    stating menu option routes

--- phase-1/phase_1.py
+++ phase-1/phase_1.py
@@ -5,3 +5,15 @@ print("1. New Lifting Equipment")
 print("2. New Customer")
 print("3. New Lifting Equipment Rental")
 print("4. Exit")
+
+# option
+if option<1 or option>4:
+    print("Error: invalid option")
+elif option==1:
+    print("New Lifting Equipment")
+elif option==2:
+    print("New Customer")
+elif option==3:
+    print("New Lifting Equipment")
+elif option==4:
+    print("Exit")
