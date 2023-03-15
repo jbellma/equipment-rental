c92fa9c7a7df727f5b093f095abe38cef260f1ed
Jose Bello <bellojosema11@gmail.com>
2023-03-15 17:49:16 +0000

    new price after VAT variable

--- phase-1/phase_1.py
+++ phase-1/phase_1.py
@@ -15,19 +15,24 @@ option = int(input("Please select an option (1-4): "))
 # It displays an error message if the option that was entered is invalid.
 if option<1 or option>4:
     print("Error: invalid option")
+
 # Prints the name of the option to run and calls the function it references if the inserted option is valid.
 elif option==1:
-    print("New Lifting Equipment")
+    print("NEW LIFTING EQUIPMENT")
     equipment_id = int(input("Please provide the identifier for the lifting equipment: "))
     equipment_brand = input("Please provide the lifting equipment's brand name: ")
     equipment_model = input("Please specify the model's name of the lifting apparatus: ")
     equipment_type = input("Please specify the equipment type: ")
     equipment_description = input("Please enter the lifting equipment's description here: ")
     equipment_price_before_vat = int(input("Please input the cost of the lifting equipment, excluding VAT: "))
+    equipment_price_after_vat = equipment_price_before_vat + equipment_price_before_vat*(VAT/100)
     equipment_units_in_stock = int(input("Please specify the number of lifting equipment units that are currently in stock: "))
+
 elif option==2:
-    print("New Customer")
+    print("NEW CUSTOMER")
+
 elif option==3:
-    print("New Lifting Equipment")
+    print("NEW LIFTING EQUIPMENT RENTAL")
+
 elif option==4:
-    print("Exit")
+    print("EXIT")
